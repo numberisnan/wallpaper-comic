8b0b84a049b6ce29debd831598afc8ed1fda2809
farazkaleemmalik <43715001+xxxFarazxxx@users.noreply.github.com>
2019-01-01 10:20:42 -0500

    Formatting

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const wallpaper = require("wallpaper");
 const fs = require("fs");
 const rp = require("request-promise-native");
 const request = require("request");
-const {parse} = require("node-html-parser");
+const { parse } = require("node-html-parser");
 const sharp = require("sharp");
 const config = require("./config");
-const {convert} = require("easyimage")
+const { convert } = require("easyimage")
 
 const dateString = (function createForamttedDate() {
     const date = new Date();
@@ -17,30 +17,30 @@ const resizedImageName = imageName.replace(/\.\w*/g, "_resized.jpeg");
 
 (async function main() {
     console.log("Requesting image url ...");
-    const parsedPage = parse(await rp("https://www.gocomics.com/garfield/"+ dateString)
-        .catch(err =>  {
-            console.log("Request failed\n",err);
+    const parsedPage = parse(await rp("https://www.gocomics.com/garfield/" + dateString)
+        .catch(err => {
+            console.log("Request failed\n", err);
             setTimeout(main, config.settings.requestRetryRate);
         })
     );
-    
+
     const imgUrl = parsedPage.querySelector(".item-comic-image img").rawAttrs.split(/ src=/)[1].replace(/"/g, "");
-    
+
     console.log("Requesting image ...");
-    await new Promise(function(resolve) { //From https://github.com/request/request-promise/issues/90
-        request.get(imgUrl).on('response', function(response) {
+    await new Promise(function (resolve) { //From https://github.com/request/request-promise/issues/90
+        request.get(imgUrl).on('response', function (response) {
             response.pause();
-            resolve(response);  
+            resolve(response);
         });
-    }).then(async function(response) {
+    }).then(async function (response) {
         var stream = response.pipe(fs.createWriteStream(imageName));
         console.log("Writing to disk ...")
-        await new Promise(function(resolve,reject){
+        await new Promise(function (resolve, reject) {
             stream.on('close', () => resolve())
             stream.on('error', () => reject())
         })
-    }).catch(err =>  {
-        console.log("Request failed\n",err);
+    }).catch(err => {
+        console.log("Request failed\n", err);
         setTimeout(main, config.settings.requestRetryRate);
     });
 
@@ -52,17 +52,18 @@ const resizedImageName = imageName.replace(/\.\w*/g, "_resized.jpeg");
 
     console.log("Resizing image ...");
     await sharp(imageConvertedName)
-    .resize(config.settings.screenResolution[0], config.settings.screenResolution[1], {fit: "contain"})
-    .toFile(resizedImageName);
+        .resize(config.settings.screenResolution[0], config.settings.screenResolution[1], { fit: "contain" })
+        .toFile(resizedImageName);
 
     console.log("Setting wallpaper ...");
     await wallpaper.set(resizedImageName);
 
+    // Use Bufffers instead of fs
     console.log("Deleting extra images ...");
     fs.unlinkSync(imageName);
 })()
-.then(() => {
-    console.log("Done!");
-    process.exit();
-})
-.catch(console.log);
+    .then(() => {
+        console.log("Done!");
+        process.exit();
+    })
+    .catch(console.log);
